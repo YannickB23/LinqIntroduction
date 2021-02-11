ac143a5f4d39cf8514662dbe37ad6770af349517
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2021-02-11 15:15:53 +0100

    QuerySyntax, MethodSyntax...

--- Features/Program.cs
+++ Features/Program.cs
@@ -36,21 +36,57 @@ namespace Features
             */
             #endregion
 
+            Func<int, int> square = x => x * x;
+            Func<int, int, int> add = (x, y) => x + y;
+
+            //zelfde add als boven, andere syntax:
+            /*Func<int, int, int> add = (x, y) =>
+            {
+                int temp = x + y;
+                return temp;
+            };*/
+
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(square(add(3, 5)));
+
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Reggie"},
-                new Employee {Id = 2, Name = "Chris"}
+                new Employee {Id = 2, Name = "Chris"},
+                new Employee {Id = 5, Name = "LeBron"}
             };
 
-            IEnumerable<Employee> sales = new List<Employee>()
+            var sales = new List<Employee>()
             {
-                new Employee { Id = 3, Name = "Rik"}
+                new Employee { Id = 3, Name = "Rik"},
+                new Employee {Id = 4, Name = "Bob"}
             };
 
-            foreach (var employee in developers.Where(e => e.Name.StartsWith("R")))
+            /*foreach (var employee in developers.Where(e => e.Name.Length == 6)
+                .OrderBy(e => e.Name))
+            {
+                Console.WriteLine(employee.Name);
+            }*/
+
+            //METHOD SYNTAX -> zelfde foreach als hierboven:
+            /*var query = developers.Where(e => e.Name.Length == 6)
+                .OrderByDescending(e => e.Name);
+            foreach (var employee in query)
+            {
+                Console.WriteLine(employee.Name);
+            }*/
+
+            //QUERY SYNTAX -> zelfde foreach als hierboven:
+            var query2 = from developer in developers
+                         where developer.Name.Length == 6
+                         orderby developer.Name descending
+                         select developer;
+            foreach (var employee in query2)
             {
                 Console.WriteLine(employee.Name);
             }
+
 
             /*Console.WriteLine(developers.Count());
             //hard way foreach => behind the scenes => outcomment using System.Linq
@@ -60,9 +96,10 @@ namespace Features
                 Console.WriteLine(enumerator.Current.Name);
             }
             */
+            Console.ReadLine();
         }
 
-        private static bool NameStartsWithS(Employee employee)
+        private static bool NameStartsWithR(Employee employee)
         {
             return employee.Name.StartsWith("R");
         }
---
+++ QuerySyntax/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = MyLinq.Random().Where(n => n > 0.5).Take(10);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            var movies = new List<Movie>
+            {
+                new Movie{Title = "The Dark Knight", Rating = 8.9f, Year =2008 },
+                new Movie{Title= "The King's Speech", Rating = 8.0f, Year = 2010},
+                new Movie{Title= "Casablanca", Rating = 88.5f, Year = 1942},
+                new Movie{Title= "Star Wars V", Rating = 8.7f, Year = 1980},
+            };
+
+            //var query = movies.Where(m => m.Year > 2000)
+            //    .OrderBy(m => m.Rating);
+
+            var query = from movie in movies
+                        where movie.Year > 2000
+                        orderby movie.Rating descending
+                        select movie;
+
+            var enumartor = query.GetEnumerator();
+            while (enumartor.MoveNext())
+            {
+                Console.WriteLine(enumartor.Current.Title);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
